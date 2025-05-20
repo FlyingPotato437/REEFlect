9f36bc8b4229981176911b1be9ecbb5d23aa050a
Sriki <srikanthsamy1@Srikanths-MacBook-Pro.local>
2025-05-19 22:29:17 -0700

    Fix: Update Team.stories.ts to use correct data source

--- src/stories/widgets/Team.stories.ts
+++ src/stories/widgets/Team.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Component from '~/components/widgets/Team';
-import { teamHome as mockData } from '~/shared/data/pages/home.data';
+import { teamAbout as mockData } from '~/shared/data/pages/about.data';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
